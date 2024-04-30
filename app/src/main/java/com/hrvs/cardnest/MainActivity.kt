0ee29293dc238833eac94be01ddbdbc58960d1c5
Harsh Vyas <kumarvyasss@gmail.com>
2024-04-30 11:28:36 +0530

    Add colors and remove redundant code

--- app/src/main/java/com/hrvs/cardnest/MainActivity.kt
+++ app/src/main/java/com/hrvs/cardnest/MainActivity.kt
@@ -4,40 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.LinearGradientShader
 import androidx.compose.ui.tooling.preview.Preview
 import com.hrvs.cardnest.ui.theme.CardNestTheme
+import com.hrvs.cardnest.ui.theme.TH_BLACK
+import com.hrvs.cardnest.ui.theme.TH_DARKER_BLUE
+import com.hrvs.cardnest.ui.theme.TH_WHITE
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			CardNestTheme {
-				// A surface container using the 'background' color from the theme
-				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-					Greeting("Android")
-				}
-			}
+			App()
 		}
 	}
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
-	)
+fun App() {
+	CardNestTheme {
+		Surface(modifier = Modifier.fillMaxSize(), color = TH_BLACK) {
+			LinearGradientShader(
+				from = Offset(0f, 0f),
+				to = Offset(100f, 100f),
+				colors = listOf(TH_BLACK, TH_DARKER_BLUE),
+			)
+			Text(text = "Hello, Harsh Vyas!", color = TH_WHITE)
+		}
+	}
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
-	CardNestTheme {
-		Greeting("Android")
-	}
+	App()
 }
--- app/src/main/java/com/hrvs/cardnest/ui/theme/Color.kt
+++ app/src/main/java/com/hrvs/cardnest/ui/theme/Color.kt
@@ -2,10 +2,34 @@ package com.hrvs.cardnest.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val TH_BLACK = Color(0xFF00060C)
+val TH_BLACK_00 = Color(0x0000060C)
+val TH_BLACK_20 = Color(0x3300060C)
+val TH_BLACK_40 = Color(0x6600060C)
+val TH_BLACK_80 = Color(0xCC00060C)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val TH_DARKER_BLUE = Color(0xFF001528)
+
+val TH_SKY = Color(0xFF3FA1FF)
+val TH_SKY_10 = Color(0x193FA1FF)
+val TH_SKY_20 = Color(0x333FA1FF)
+
+val TH_RED = Color(0xFFF31260)
+val TH_RED_10 = Color(0x19F31260)
+val TH_RED_12 = Color(0x1FF31260)
+val TH_RED_15 = Color(0x26F31260)
+val TH_RED_60 = Color(0x99F31260)
+
+val TH_WHITE = Color(0xFFF4F7FB)
+val TH_WHITE_00 = Color(0x00F4F7FB)
+val TH_WHITE_05 = Color(0x0DF4F7FB)
+val TH_WHITE_07 = Color(0x12F4F7FB)
+val TH_WHITE_10 = Color(0x19F4F7FB)
+val TH_WHITE_20 = Color(0x33F4F7FB)
+val TH_WHITE_50 = Color(0x7FF4F7FB)
+val TH_WHITE_60 = Color(0x99F4F7FB)
+val TH_WHITE_65 = Color(0xA6F4F7FB)
+val TH_WHITE_70 = Color(0xB2F4F7FB)
+val TH_WHITE_80 = Color(0xCCF4F7FB)
+val TH_WHITE_85 = Color(0xD9F4F7FB)
+val TH_WHITE_90 = Color(0xE6F4F7FB)
