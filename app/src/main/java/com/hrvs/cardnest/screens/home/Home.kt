b4f925945294c8b1c619e15dbedea6a1b624d077
Harsh Vyas <kumarvyasss@gmail.com>
2024-05-02 23:56:34 +0530

    Move app to screens directory

---
+++ app/src/main/java/com/hrvs/cardnest/screens/home/Home.kt
@@ -0,0 +1,48 @@
+package com.hrvs.cardnest.screens.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+import com.hrvs.cardnest.components.card.CardPreview
+import com.hrvs.cardnest.components.header.HeaderSearch
+import com.hrvs.cardnest.components.header.HeaderTitle
+import com.hrvs.cardnest.data.CardFullProfile
+import com.hrvs.cardnest.data.CardTheme
+import com.hrvs.cardnest.data.PaymentNetwork
+import com.hrvs.cardnest.ui.theme.ScreenContainer
+import com.hrvs.cardnest.ui.theme.TabScreenRoot
+
+val cards = listOf(
+  CardFullProfile(
+    number = "1234567890123456",
+    expiry = "10/28",
+    cardholder = "John Doe",
+    issuer = "Bank of America",
+    network = PaymentNetwork.VISA,
+    theme = CardTheme.ROSE
+  ),
+  CardFullProfile(
+    number = "4037720873290682",
+    expiry = "12/25",
+    cardholder = "Jane Doe",
+    issuer = "Chase",
+    network = PaymentNetwork.MASTERCARD,
+    theme = CardTheme.PURPLE
+  )
+)
+
+class HomeScreen : Screen {
+
+  @Preview
+  @Composable
+  override fun Content() {
+    TabScreenRoot {
+      HeaderTitle("Home")
+      HeaderSearch()
+      ScreenContainer(16.dp) {
+        cards.forEach { CardPreview(it) }
+      }
+    }
+  }
+}
---
+++ app/src/main/java/com/hrvs/cardnest/screens/home/card/CardView.kt
@@ -0,0 +1,19 @@
+package com.hrvs.cardnest.screens.home.card
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+import com.hrvs.cardnest.components.card.CardPreview
+import com.hrvs.cardnest.data.CardFullProfile
+import com.hrvs.cardnest.ui.theme.ScreenContainer
+import com.hrvs.cardnest.ui.theme.TabScreenRoot
+
+data class CardViewScreen(val card: CardFullProfile) : Screen {
+  @Composable
+  override fun Content() {
+    TabScreenRoot {
+      ScreenContainer {
+        CardPreview(card)
+      }
+    }
+  }
+}
