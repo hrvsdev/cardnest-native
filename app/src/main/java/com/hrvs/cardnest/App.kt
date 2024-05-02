fa74b87d1a3a20e655079eebc3ff2a4d98f7c86c
Harsh Vyas <kumarvyasss@gmail.com>
2024-05-02 12:11:11 +0530

    Use moved components in `App`

--- app/src/main/java/com/hrvs/cardnest/App.kt
+++ app/src/main/java/com/hrvs/cardnest/App.kt
@@ -1,106 +1,44 @@
 package com.hrvs.cardnest
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Clear
-import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.hrvs.cardnest.components.CardPreview
-import com.hrvs.cardnest.ui.theme.CardNestTheme
-import com.hrvs.cardnest.ui.theme.TH_BLACK
-import com.hrvs.cardnest.ui.theme.TH_WHITE
-import com.hrvs.cardnest.ui.theme.TH_WHITE_07
-import com.hrvs.cardnest.ui.theme.TH_WHITE_10
-import com.hrvs.cardnest.ui.theme.TH_WHITE_60
+import com.hrvs.cardnest.components.card.CardPreview
+import com.hrvs.cardnest.components.header.HeaderSearch
+import com.hrvs.cardnest.components.header.HeaderTitle
+import com.hrvs.cardnest.data.CardFullProfile
+import com.hrvs.cardnest.data.CardTheme
+import com.hrvs.cardnest.data.PaymentNetwork
+import com.hrvs.cardnest.ui.theme.ScreenContainer
+import com.hrvs.cardnest.ui.theme.TabScreenRoot
 
 @Preview
 @Composable
 fun App() {
-  val (search, onSearchChange) = remember { mutableStateOf("") }
-
-  CardNestTheme {
-    Surface(Modifier.fillMaxSize(), color = TH_BLACK) {
-      Column {
-        Box(Modifier.padding(top = 32.dp, start = 18.dp, end = 16.dp)) {
-          Text(
-            text = "Home",
-            color = TH_WHITE,
-            fontSize = 28.sp,
-            fontWeight = FontWeight(600),
-            lineHeight = 34.sp
-          )
-        }
-        Box(Modifier.padding(16.dp)) {
-          TextField(
-            singleLine = true,
-            value = search,
-            onValueChange = onSearchChange,
-            placeholder = {
-              Text(
-                text = "Enter card number, bank or network",
-                color = TH_WHITE_60,
-                fontSize = 16.sp,
-                lineHeight = 20.sp
-              )
-            },
-            prefix = {
-              Icon(
-                imageVector = Icons.Outlined.Search,
-                contentDescription = "Search",
-                Modifier
-                  .padding(end = 8.dp)
-                  .size(28.dp),
-                tint = TH_WHITE_60
-              )
-            },
-            suffix = {
-              if (search.isNotEmpty()) {
-                IconButton(onClick = { onSearchChange("") }) {
-                  Icon(
-                    imageVector = Icons.Outlined.Clear,
-                    contentDescription = "Clear",
-                    tint = TH_WHITE_60
-                  )
-                }
-              }
-            },
-            textStyle = TextStyle(color = TH_WHITE, fontSize = 16.sp),
-            modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(14.dp),
-            colors = TextFieldDefaults.colors(
-              unfocusedContainerColor = TH_WHITE_07,
-              focusedContainerColor = TH_WHITE_10,
-              focusedIndicatorColor = Color.Transparent,
-              unfocusedIndicatorColor = Color.Transparent,
-            )
-          )
-        }
-        Column(Modifier.padding(16.dp), Arrangement.spacedBy(16.dp)) {
-          CardPreview(cardNumber = "6383 8737 7637 5373", expiry = "08/30")
-        }
-      }
+  TabScreenRoot {
+    HeaderTitle("Home")
+    HeaderSearch()
+    ScreenContainer(16.dp) {
+      CardPreview(
+        CardFullProfile(
+          number = "1234567890123456",
+          expiry = "10/28",
+          cardholder = "John Doe",
+          issuer = "Bank of America",
+          network = PaymentNetwork.VISA,
+          theme = CardTheme.ROSE
+        )
+      )
+      CardPreview(
+        CardFullProfile(
+          number = "4037720873290682",
+          expiry = "12/25",
+          cardholder = "Jane Doe",
+          issuer = "Chase",
+          network = PaymentNetwork.MASTERCARD,
+          theme = CardTheme.PURPLE
+        )
+      )
     }
   }
 }
