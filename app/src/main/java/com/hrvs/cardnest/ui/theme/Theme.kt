86fae81c602082d7298446e3dd7e55761fbb465c
Harsh Vyas <kumarvyasss@gmail.com>
2024-05-02 12:09:31 +0530

    Add `TabScreenRoot` and `ScreenContainer`

--- app/src/main/java/com/hrvs/cardnest/ui/theme/Theme.kt
+++ app/src/main/java/com/hrvs/cardnest/ui/theme/Theme.kt
@@ -1,11 +1,19 @@
 package com.hrvs.cardnest.ui.theme
 
 import android.app.Activity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 
 @Composable
@@ -24,3 +32,23 @@ fun CardNestTheme(content: @Composable () -> Unit) {
   )
 }
 
+@Composable
+fun TabScreenRoot(content: @Composable () -> Unit) {
+  CardNestTheme {
+    Surface(Modifier.fillMaxSize(), color = TH_BLACK) {
+      Column {
+        content()
+      }
+    }
+  }
+}
+
+@Composable
+fun ScreenContainer(spacedBy: Dp? = null, content: @Composable () -> Unit) {
+  Column(
+    modifier = Modifier.padding(16.dp),
+    verticalArrangement = if (spacedBy != null) Arrangement.spacedBy(spacedBy) else Arrangement.Top
+  ) {
+    content()
+  }
+}
