86dbf355edc70981fc2b8f22b14813e463393a14
Harsh Vyas <kumarvyasss@gmail.com>
2024-05-05 13:45:56 +0530

    Remove unused imports

--- app/src/main/java/com/hrvs/cardnest/components/header/HeaderSearch.kt
+++ app/src/main/java/com/hrvs/cardnest/components/header/HeaderSearch.kt
@@ -22,7 +22,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
