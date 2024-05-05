b1ea0174181ec106c5bdd2004133391585b760fb
Harsh Vyas <kumarvyasss@gmail.com>
2024-05-05 13:45:32 +0530

    Add `AppTextField` to `CardView`

--- app/src/main/java/com/hrvs/cardnest/screens/home/card/CardView.kt
+++ app/src/main/java/com/hrvs/cardnest/screens/home/card/CardView.kt
@@ -1,8 +1,14 @@
 package com.hrvs.cardnest.screens.home.card
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import com.hrvs.cardnest.components.card.CardPreview
+import com.hrvs.cardnest.components.core.AppTextField
 import com.hrvs.cardnest.components.header.SubScreenHeader
 import com.hrvs.cardnest.data.CardFullProfile
 import com.hrvs.cardnest.ui.theme.ScreenContainer
@@ -11,10 +17,21 @@ import com.hrvs.cardnest.ui.theme.TabScreenRoot
 data class CardViewScreen(val card: CardFullProfile) : Screen {
   @Composable
   override fun Content() {
+    val (number, onNumberChange) = remember { mutableStateOf("") }
+
     TabScreenRoot {
       SubScreenHeader("Card")
-      ScreenContainer {
+      ScreenContainer(32.dp) {
         CardPreview(card)
+        Column(verticalArrangement = Arrangement.spacedBy(24.dp)) {
+          AppTextField(
+            label = "Card number",
+            placeholder = "Enter card number",
+            value = number,
+            onValueChange = onNumberChange,
+            maxLength = 16,
+          )
+        }
       }
     }
   }
--- app/src/main/java/com/hrvs/cardnest/ui/theme/Color.kt
+++ app/src/main/java/com/hrvs/cardnest/ui/theme/Color.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package com.hrvs.cardnest.ui.theme
 
 import androidx.compose.ui.graphics.Color
