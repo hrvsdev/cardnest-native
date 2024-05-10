5ae33f5c8205519acc0e1ddcd712a2e4b7388864
Harsh Vyas <kumarvyasss@gmail.com>
2024-05-10 13:40:33 +0530

    Add `CardThemeSelector` to `CardView`

--- app/src/main/java/com/hrvs/cardnest/screens/home/card/CardView.kt
+++ app/src/main/java/com/hrvs/cardnest/screens/home/card/CardView.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import com.hrvs.cardnest.components.card.CardPreview
+import com.hrvs.cardnest.components.card.CardThemeSelector
 import com.hrvs.cardnest.components.core.AppTextField
 import com.hrvs.cardnest.components.header.SubScreenHeader
 import com.hrvs.cardnest.data.CardFullProfile
@@ -18,19 +19,43 @@ data class CardViewScreen(val card: CardFullProfile) : Screen {
   @Composable
   override fun Content() {
     val (number, onNumberChange) = remember { mutableStateOf("") }
+    val (expiry, onExpiryChange) = remember { mutableStateOf("") }
+    val (cardholder, onCardholderChange) = remember { mutableStateOf("") }
+    val (issuer, onIssuerChange) = remember { mutableStateOf("") }
+
+    val (theme, onThemeChange) = remember { mutableStateOf(card.theme) }
 
     TabScreenRoot {
       SubScreenHeader("Card")
       ScreenContainer(32.dp) {
-        CardPreview(card)
+        CardPreview(card.copy(theme = theme))
         Column(verticalArrangement = Arrangement.spacedBy(24.dp)) {
           AppTextField(
             label = "Card number",
             placeholder = "Enter card number",
             value = number,
             onValueChange = onNumberChange,
-            maxLength = 16,
           )
+          AppTextField(
+            label = "Expiry date",
+            placeholder = "Enter expiry date",
+            value = expiry,
+            onValueChange = onExpiryChange,
+          )
+          AppTextField(
+            label = "Cardholder name",
+            placeholder = "Enter cardholder name",
+            value = cardholder,
+            onValueChange = onCardholderChange,
+          )
+          AppTextField(
+            label = "Issuer",
+            placeholder = "Enter card issuer/bank",
+            value = issuer,
+            onValueChange = onIssuerChange,
+          )
+
+          CardThemeSelector(theme, onThemeChange)
         }
       }
     }
