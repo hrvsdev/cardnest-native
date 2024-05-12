0a7c3b5159d92ae6a34ed28ed2c66942dd132163
Harsh Vyas <kumarvyasss@gmail.com>
2024-05-12 13:19:38 +0530

    Refactor `CardView`

--- app/src/main/java/com/hrvs/cardnest/screens/home/card/CardView.kt
+++ app/src/main/java/com/hrvs/cardnest/screens/home/card/CardView.kt
@@ -1,10 +1,16 @@
 package com.hrvs.cardnest.screens.home.card
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text2.input.InputTransformation
+import androidx.compose.foundation.text2.input.rememberTextFieldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import com.hrvs.cardnest.components.card.CardPreview
@@ -16,12 +22,13 @@ import com.hrvs.cardnest.ui.theme.ScreenContainer
 import com.hrvs.cardnest.ui.theme.TabScreenRoot
 
 data class CardViewScreen(val card: CardFullProfile) : Screen {
+  @OptIn(ExperimentalFoundationApi::class)
   @Composable
   override fun Content() {
-    val (number, onNumberChange) = remember { mutableStateOf("") }
-    val (expiry, onExpiryChange) = remember { mutableStateOf("") }
-    val (cardholder, onCardholderChange) = remember { mutableStateOf("") }
-    val (issuer, onIssuerChange) = remember { mutableStateOf("") }
+    val number = rememberTextFieldState(card.number.chunked(4).joinToString(" "))
+    val expiry = rememberTextFieldState(card.expiry)
+    val cardholder = rememberTextFieldState(card.cardholder)
+    val issuer = rememberTextFieldState(card.issuer)
 
     val (theme, onThemeChange) = remember { mutableStateOf(card.theme) }
 
@@ -33,26 +40,37 @@ data class CardViewScreen(val card: CardFullProfile) : Screen {
           AppTextField(
             label = "Card number",
             placeholder = "Enter card number",
-            value = number,
-            onValueChange = onNumberChange,
+            state = number,
+            inputTransformation = CardNumberTransformation,
+            keyboardOptions = KeyboardOptions.Default.copy(
+              imeAction = ImeAction.Next,
+              keyboardType = KeyboardType.Number,
+            ),
           )
+
           AppTextField(
             label = "Expiry date",
             placeholder = "Enter expiry date",
-            value = expiry,
-            onValueChange = onExpiryChange,
+            state = expiry,
+            inputTransformation = ExpiryTransformation,
+            keyboardOptions = KeyboardOptions.Default.copy(
+              imeAction = ImeAction.Next,
+              keyboardType = KeyboardType.Number,
+            ),
           )
+
           AppTextField(
             label = "Cardholder name",
             placeholder = "Enter cardholder name",
-            value = cardholder,
-            onValueChange = onCardholderChange,
+            state = cardholder,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
           )
+
           AppTextField(
             label = "Issuer",
             placeholder = "Enter card issuer/bank",
-            value = issuer,
-            onValueChange = onIssuerChange,
+            state = issuer,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next)
           )
 
           CardThemeSelector(theme, onThemeChange)
@@ -60,4 +78,31 @@ data class CardViewScreen(val card: CardFullProfile) : Screen {
       }
     }
   }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+val CardNumberTransformation = InputTransformation.byValue { _, proposed ->
+  val filteredValue = proposed.filter { c -> c.isDigit() }.take(16)
+  val formattedValue = filteredValue.chunked(4).joinToString(" ")
+
+  formattedValue
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+val ExpiryTransformation = InputTransformation.byValue { current, proposed ->
+  println()
+
+  var filteredValue = proposed.filter { c -> c.isDigit() }.take(4)
+
+  if (filteredValue.isNotEmpty() && filteredValue[0].toString().toInt() > 1) {
+    filteredValue = "0$filteredValue"
+  }
+
+  if (current.endsWith("/") && filteredValue.length == 2) {
+    filteredValue = proposed
+  } else if (filteredValue.length >= 2) {
+    filteredValue = filteredValue.substring(0, 2) + "/" + filteredValue.substring(2)
+  }
+
+  filteredValue
 }
