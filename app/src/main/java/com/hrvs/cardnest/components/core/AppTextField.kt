d3b8007abbb09e09808cfd2b574b7d4eac041b4f
Harsh Vyas <kumarvyasss@gmail.com>
2024-05-14 14:21:14 +0530

    Add `CopyableTextField`

--- app/src/main/java/com/hrvs/cardnest/components/core/AppTextField.kt
+++ app/src/main/java/com/hrvs/cardnest/components/core/AppTextField.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -14,16 +16,28 @@ import androidx.compose.foundation.text2.BasicTextField2
 import androidx.compose.foundation.text2.input.InputTransformation
 import androidx.compose.foundation.text2.input.TextFieldLineLimits
 import androidx.compose.foundation.text2.input.TextFieldState
+import androidx.compose.foundation.text2.input.rememberTextFieldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.hrvs.cardnest.R
 import com.hrvs.cardnest.ui.theme.AppText
 import com.hrvs.cardnest.ui.theme.AppTextSize
 import com.hrvs.cardnest.ui.theme.TH_RED
@@ -32,6 +46,7 @@ import com.hrvs.cardnest.ui.theme.TH_WHITE
 import com.hrvs.cardnest.ui.theme.TH_WHITE_07
 import com.hrvs.cardnest.ui.theme.TH_WHITE_10
 import com.hrvs.cardnest.ui.theme.TH_WHITE_60
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -41,9 +56,12 @@ fun AppTextField(
   label: String? = null,
   placeholder: String? = null,
   error: String? = null,
+  readOnly: Boolean = false,
 
   inputTransformation: InputTransformation? = null,
-  keyboardOptions: KeyboardOptions = KeyboardOptions. Default,
+  keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+
+  rightIcon: @Composable (() -> Unit)? = null,
 
   onFocus: () -> Unit = {},
   onBlur: () -> Unit = {},
@@ -54,18 +72,18 @@ fun AppTextField(
     if (label != null) AppText(label, Modifier.padding(start = 8.dp))
 
     BasicTextField2(
-      state,
+      state = state,
       textStyle = TextStyle(if (error.isNullOrEmpty()) TH_WHITE else TH_RED, fontSize = 16.sp),
       cursorBrush = SolidColor(TH_SKY_80),
 
       lineLimits = TextFieldLineLimits.SingleLine,
+      readOnly = readOnly,
 
       inputTransformation = inputTransformation,
       keyboardOptions = keyboardOptions,
 
       modifier = Modifier
         .fillMaxWidth()
-        .height(48.dp)
         .background(if (isFocused.value) TH_WHITE_10 else TH_WHITE_07, RoundedCornerShape(14.dp))
         .onFocusChanged {
           isFocused.value = it.isFocused
@@ -73,19 +91,67 @@ fun AppTextField(
         },
 
       decorator = { innerTextField ->
-        Column(Modifier.padding(start = 16.dp, end = 16.dp), Arrangement.Center) {
-          Box(contentAlignment = Alignment.CenterStart) {
-            if (state.text.isEmpty() && placeholder != null) {
-              AppText(text = placeholder, color = TH_WHITE_60)
+        Box(Modifier.padding(start = 16.dp, end = if (rightIcon == null) 16.dp else 0.dp)) {
+          Row(Modifier.fillMaxWidth(), Arrangement.SpaceBetween, Alignment.CenterVertically) {
+            Box(Modifier.height(48.dp), contentAlignment = Alignment.CenterStart) {
+              if (state.text.isEmpty() && placeholder != null) {
+                AppText(text = placeholder, color = TH_WHITE_60)
+              }
+              innerTextField()
             }
-            innerTextField()
+
+            if (rightIcon != null) rightIcon()
           }
         }
-      }
-    )
+      })
 
     if (error != null) AppText(
       error, Modifier.padding(start = 8.dp), AppTextSize.SM, color = TH_RED
     )
   }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun CopyableTextField(
+  text: String,
+  label: String? = null,
+  textToCopy: String = text,
+) {
+  val clipboard = LocalClipboardManager.current
+
+  val textState = rememberTextFieldState(text)
+
+  var hasCopied by remember { mutableStateOf(false) }
+  var offsetY by remember { mutableStateOf(0.dp) }
+
+  fun copyText() {
+    hasCopied = true
+    clipboard.setText(AnnotatedString(textToCopy))
+  }
+
+  LaunchedEffect(hasCopied) {
+    if (hasCopied) {
+      offsetY = 1.dp
+      delay(200)
+      offsetY = 0.dp
+      delay(1500)
+      hasCopied = false
+    }
+  }
+
+  AppTextField(
+    state = textState,
+    label = label,
+    readOnly = true,
+    rightIcon = {
+      IconButton(::copyText, Modifier.offset(y = offsetY), !hasCopied) {
+        if (hasCopied) {
+          Icon(Icons.Default.Check, "Text copied", tint = TH_WHITE)
+        } else {
+          Icon(painterResource(R.drawable.outline_content_copy_24), "Copy text", tint = TH_WHITE)
+        }
+      }
+    }
+  )
 }
