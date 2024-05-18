10304fa763c93260a7359a0b8d6c52acaa189832
Harsh Vyas <kumarvyasss@gmail.com>
2024-05-18 13:53:03 +0530

    Add `CardEditorViewModel` to `CardEditor`

--- app/src/main/java/com/hrvs/cardnest/components/card/CardEditor.kt
+++ app/src/main/java/com/hrvs/cardnest/components/card/CardEditor.kt
@@ -5,55 +5,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text2.input.InputTransformation
-import androidx.compose.foundation.text2.input.TextFieldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.hrvs.cardnest.components.core.AppTextField
 import com.hrvs.cardnest.data.CardFocusableField
-import com.hrvs.cardnest.data.CardFullProfile
-import com.hrvs.cardnest.data.CardTheme
-import com.hrvs.cardnest.data.PaymentNetwork
+import com.hrvs.cardnest.state.card.CardEditorViewModel
 import com.hrvs.cardnest.utils.card.addCardNumberSpaces
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun CardEditor(
-  number: TextFieldState,
-  expiry: TextFieldState,
-  cardholder: TextFieldState,
-  issuer: TextFieldState,
+fun CardEditor(viewModel: CardEditorViewModel) {
+  CardPreview(viewModel.card, usePlaceholders = true, focused = viewModel.focused.value)
 
-  network: PaymentNetwork,
-  onNetworkChange: (PaymentNetwork) -> Unit,
-
-  theme: CardTheme,
-  onThemeChange: (CardTheme) -> Unit,
-
-  focused: CardFocusableField?,
-  onFocusedChange: (CardFocusableField?) -> Unit,
-) {
-  CardPreview(
-    card = CardFullProfile(
-      number = number.text.toString(),
-      expiry = expiry.text.toString(),
-      cardholder = cardholder.text.toString(),
-      issuer = issuer.text.toString(),
-      network = network,
-      theme = theme,
-    ),
-    usePlaceholders = true,
-    focused = focused,
-  )
   Column(verticalArrangement = Arrangement.spacedBy(24.dp)) {
     AppTextField(
       label = "Card number",
       placeholder = "Enter card number",
-      state = number,
+      state = viewModel.number,
       inputTransformation = CardNumberTransformation,
-      onFocus = { onFocusedChange(CardFocusableField.NUMBER) },
-      onBlur = { onFocusedChange(null) },
+      onFocus = { viewModel.onFocusedChange(CardFocusableField.NUMBER) },
+      onBlur = { viewModel.onFocusedChange(null) },
       keyboardOptions = KeyboardOptions.Default.copy(
         imeAction = ImeAction.Next,
         keyboardType = KeyboardType.Number,
@@ -63,10 +36,10 @@ fun CardEditor(
     AppTextField(
       label = "Expiry date",
       placeholder = "Enter expiry date",
-      state = expiry,
+      state = viewModel.expiry,
       inputTransformation = ExpiryTransformation,
-      onFocus = { onFocusedChange(CardFocusableField.EXPIRY) },
-      onBlur = { onFocusedChange(null) },
+      onFocus = { viewModel.onFocusedChange(CardFocusableField.EXPIRY) },
+      onBlur = { viewModel.onFocusedChange(null) },
       keyboardOptions = KeyboardOptions.Default.copy(
         imeAction = ImeAction.Next,
         keyboardType = KeyboardType.Number,
@@ -76,22 +49,22 @@ fun CardEditor(
     AppTextField(
       label = "Cardholder name",
       placeholder = "Enter cardholder name",
-      state = cardholder,
-      onFocus = { onFocusedChange(CardFocusableField.CARDHOLDER) },
-      onBlur = { onFocusedChange(null) },
+      state = viewModel.cardholder,
+      onFocus = { viewModel.onFocusedChange(CardFocusableField.CARDHOLDER) },
+      onBlur = { viewModel.onFocusedChange(null) },
       keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
     )
 
     AppTextField(
       label = "Issuer",
       placeholder = "Enter card issuer/bank",
-      state = issuer,
-      onFocus = { onFocusedChange(CardFocusableField.ISSUER) },
-      onBlur = { onFocusedChange(null) },
+      state = viewModel.issuer,
+      onFocus = { viewModel.onFocusedChange(CardFocusableField.ISSUER) },
+      onBlur = { viewModel.onFocusedChange(null) },
       keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next)
     )
 
-    CardThemeSelector(theme, onThemeChange)
+    CardThemeSelector(viewModel.theme.value, viewModel::onThemeChange)
   }
 }
 
--- app/src/main/java/com/hrvs/cardnest/screens/home/card/editor/UpdateCardEditor.kt
+++ app/src/main/java/com/hrvs/cardnest/screens/home/card/editor/UpdateCardEditor.kt
@@ -1,56 +1,25 @@
 package com.hrvs.cardnest.screens.home.card.editor
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.text2.input.TextFieldState
-import androidx.compose.foundation.text2.input.rememberTextFieldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import cafe.adriel.voyager.core.screen.Screen
 import com.hrvs.cardnest.components.card.CardEditor
 import com.hrvs.cardnest.components.header.SubScreenHeader
-import com.hrvs.cardnest.data.CardFocusableField
 import com.hrvs.cardnest.data.CardFullProfile
-import com.hrvs.cardnest.data.CardTheme
-import com.hrvs.cardnest.data.PaymentNetwork
+import com.hrvs.cardnest.state.card.CardEditorViewModel
 import com.hrvs.cardnest.ui.theme.ScreenContainer
 import com.hrvs.cardnest.ui.theme.TabScreenRoot
-import com.hrvs.cardnest.utils.card.addCardNumberSpaces
 
 data class UpdateCardEditorScreen(val card: CardFullProfile) : Screen {
-  @OptIn(ExperimentalFoundationApi::class)
   @Composable
   override fun Content() {
+    val viewModel = remember { CardEditorViewModel(card) }
+
     TabScreenRoot {
       SubScreenHeader("Edit Card", rightButtonLabel = "Done")
       ScreenContainer(32.dp) {
-        val number = rememberTextFieldState(addCardNumberSpaces(card.number))
-        val expiry = rememberTextFieldState(card.expiry)
-        val cardholder = rememberTextFieldState(card.cardholder)
-        val issuer = rememberTextFieldState(card.issuer)
-
-        val (network, onNetworkChange) = remember { mutableStateOf(card.network) }
-        val (theme, onThemeChange) = remember { mutableStateOf(card.theme) }
-
-        val (focused, onFocusedChange) = remember { mutableStateOf<CardFocusableField?>(null) }
-
-        CardEditor(
-          number = number,
-          expiry = expiry,
-          cardholder = cardholder,
-          issuer = issuer,
-
-          network = network,
-          onNetworkChange = onNetworkChange,
-
-          theme = theme,
-          onThemeChange = onThemeChange,
-
-          focused = focused,
-          onFocusedChange = onFocusedChange,
-        )
+        CardEditor(viewModel)
       }
     }
   }
