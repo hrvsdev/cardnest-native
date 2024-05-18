b5fe04f9c61c3fad67221538b0adbb18c477a77d
Harsh Vyas <kumarvyasss@gmail.com>
2024-05-18 14:15:54 +0530

    Add google fonts and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,25 +47,21 @@ android {
 }
 
 dependencies {
-
 	val voyagerVersion = "1.0.0"
 
-	implementation("androidx.core:core-ktx:1.13.0")
-	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-	implementation("androidx.activity:activity-compose:1.9.0")
-	implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+	implementation("androidx.core:core-ktx:1.13.1")
+	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+	implementation("androidx.activity:activity-compose:1.9.2")
+	implementation(platform("androidx.compose:compose-bom:2024.09.02"))
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.ui:ui-graphics")
 	implementation("androidx.compose.ui:ui-tooling-preview")
 	implementation("androidx.compose.material3:material3")
-	testImplementation("junit:junit:4.13.2")
-	androidTestImplementation("androidx.test.ext:junit:1.1.5")
-	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-	androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
-	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
 	debugImplementation("androidx.compose.ui:ui-tooling")
 	debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+	implementation("androidx.compose.ui:ui-text-google-fonts:1.7.2")
 
 	implementation("cafe.adriel.voyager:voyager-navigator:$voyagerVersion")
 	implementation("cafe.adriel.voyager:voyager-screenmodel:$voyagerVersion")
