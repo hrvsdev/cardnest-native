14d13b4067eba75d6e55a1d1e7b87bb50d3e63d5
Harsh Vyas <kumarvyasss@gmail.com>
2024-05-21 12:53:10 +0530

    Update `HeaderSearch`

--- app/src/main/java/com/hrvs/cardnest/components/header/HeaderSearch.kt
+++ app/src/main/java/com/hrvs/cardnest/components/header/HeaderSearch.kt
@@ -1,41 +1,21 @@
 package com.hrvs.cardnest.components.header
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.input.clearText
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.hrvs.cardnest.components.core.AppTextField
-import com.hrvs.cardnest.ui.theme.TH_SKY_80
-import com.hrvs.cardnest.ui.theme.TH_WHITE
-import com.hrvs.cardnest.ui.theme.TH_WHITE_07
-import com.hrvs.cardnest.ui.theme.TH_WHITE_10
 import com.hrvs.cardnest.ui.theme.TH_WHITE_60
 
 @Preview(showBackground = true, showSystemUi = true, backgroundColor = 0xFF00060C)
@@ -44,7 +24,7 @@ fun HeaderSearch() {
   val search = rememberTextFieldState("")
 
   @Composable
-  fun Prefix() {
+  fun SearchIcon() {
     Box(Modifier.size(48.dp), Alignment.Center) {
       Icon(
         imageVector = Icons.Outlined.Search,
@@ -56,9 +36,9 @@ fun HeaderSearch() {
   }
 
   @Composable
-  fun Suffix() {
+  fun ClearButton() {
     if (search.text.isNotEmpty()) {
-      IconButton(onClick = { search.edit { replace(0, search.text.length, "") } }) {
+      IconButton(onClick = { search.clearText() }) {
         Icon(
           imageVector = Icons.Outlined.Clear, contentDescription = "Clear", tint = TH_WHITE_60
         )
@@ -66,12 +46,12 @@ fun HeaderSearch() {
     }
   }
 
-  Box(Modifier.padding(horizontal = 16.dp)) {
+  Box(Modifier.padding(16.dp)) {
     AppTextField(
       state = search,
       placeholder = "Search",
-      leftIcon = { Prefix() },
-      rightIcon = { Suffix() },
+      leftIcon = { SearchIcon() },
+      rightIcon = { ClearButton() },
     )
   }
 }
