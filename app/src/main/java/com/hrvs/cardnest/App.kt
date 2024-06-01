061337a93f9d82d7d6b63b19eac3358cd1d2f170
Harsh Vyas <kumarvyasss@gmail.com>
2024-06-01 17:29:31 +0530

    Replace root navigation with tab navigation

--- app/src/main/java/com/hrvs/cardnest/App.kt
+++ app/src/main/java/com/hrvs/cardnest/App.kt
@@ -1,13 +1,74 @@
 package com.hrvs.cardnest
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
+import cafe.adriel.voyager.navigator.tab.TabOptions
 import cafe.adriel.voyager.transitions.SlideTransition
+import com.hrvs.cardnest.screens.add.AddCardScreen
 import com.hrvs.cardnest.screens.home.HomeScreen
+import com.hrvs.cardnest.screens.user.UserScreen
 
 @Composable
 fun App() {
-  Navigator(HomeScreen()) {
-    SlideTransition(it)
+  TabNavigator(HomeTab) {
+    CurrentTab()
   }
+}
+
+object HomeTab : Tab {
+  @Composable
+  override fun Content() {
+    Navigator(HomeScreen()) {
+      SlideTransition(it)
+    }
+  }
+
+  override val options: TabOptions
+    @Composable
+    get() = remember {
+      TabOptions(
+        index = 0u,
+        title = "Home",
+      )
+    }
+}
+
+object AddCardTab : Tab {
+  @Composable
+  override fun Content() {
+    Navigator(AddCardScreen()) {
+      SlideTransition(it)
+    }
+  }
+
+  override val options: TabOptions
+    @Composable
+    get() = remember {
+      TabOptions(
+        index = 1u,
+        title = "Add Card",
+      )
+    }
+}
+
+object UserTab : Tab {
+  @Composable
+  override fun Content() {
+    Navigator(UserScreen()) {
+      SlideTransition(it)
+    }
+  }
+
+  override val options: TabOptions
+    @Composable
+    get() = remember {
+      TabOptions(
+        index = 2u,
+        title = "User",
+      )
+    }
 }
