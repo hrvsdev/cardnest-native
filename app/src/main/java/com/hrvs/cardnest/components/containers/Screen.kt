9506efc20a617495f8a9621c639372e094cde99f
Harsh Vyas <kumarvyasss@gmail.com>
2024-06-03 17:05:14 +0530

    Refactor screen root containers

--- app/src/main/java/com/hrvs/cardnest/components/containers/Screen.kt
+++ app/src/main/java/com/hrvs/cardnest/components/containers/Screen.kt
@@ -3,10 +3,12 @@ package com.hrvs.cardnest.components.containers
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -23,7 +25,7 @@ val ScreenModifier = Modifier
 @Composable
 fun TabScreenRoot(content: @Composable () -> Unit) {
   Column(ScreenModifier) {
-    Column(Modifier.weight(1f)) {
+    Box(Modifier.weight(1f)) {
       content()
     }
     TabBar()
@@ -45,12 +47,9 @@ fun SubScreenRoot(
 
   content: @Composable () -> Unit
 ) {
-  LazyColumn(ScreenModifier) {
-    stickyHeader {
-      SubScreenHeader(title, leftIconLabel, rightButtonLabel, rightButtonIcon, onRightButtonClick)
-    }
-
-    item {
+  Column(ScreenModifier) {
+    SubScreenHeader(title, leftIconLabel, rightButtonLabel, rightButtonIcon, onRightButtonClick)
+    Box(Modifier.verticalScroll(rememberScrollState())) {
       ScreenContainer(spacedBy, content)
     }
   }
