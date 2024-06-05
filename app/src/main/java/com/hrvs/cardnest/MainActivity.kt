0b60f55b2dfae41ed34770313406ed794cda6f23
Harsh Vyas <kumarvyasss@gmail.com>
2024-06-05 16:24:58 +0530

    Enable edge to  edge in app

--- app/src/main/java/com/hrvs/cardnest/MainActivity.kt
+++ app/src/main/java/com/hrvs/cardnest/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.hrvs.cardnest
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -13,12 +16,13 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     installSplashScreen()
+    enableEdgeToEdge(SystemBarStyle.dark(Color.TRANSPARENT), SystemBarStyle.dark(Color.TRANSPARENT))
     setContent {
       CardNestTheme {
         App()
       }
     }
-    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
   }
 }
 
--- app/src/main/java/com/hrvs/cardnest/ui/theme/Theme.kt
+++ app/src/main/java/com/hrvs/cardnest/ui/theme/Theme.kt
@@ -1,24 +1,10 @@
 package com.hrvs.cardnest.ui.theme
 
-import android.app.Activity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 @Composable
 fun CardNestTheme(content: @Composable () -> Unit) {
-  val view = LocalView.current
-  if (!view.isInEditMode) {
-    SideEffect {
-      val window = (view.context as Activity).window
-      window.statusBarColor = TH_BLACK.toArgb()
-      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
-    }
-  }
-
   MaterialTheme(
     content = content
   )
