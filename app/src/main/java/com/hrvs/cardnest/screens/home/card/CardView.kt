fda70ca66dae5808af7209c0b3aca58fc2d788ba
Harsh Vyas <kumarvyasss@gmail.com>
2024-06-14 12:31:06 +0530

    Show delete bottom sheet on `CardView`

--- app/src/main/java/com/hrvs/cardnest/screens/home/card/CardView.kt
+++ app/src/main/java/com/hrvs/cardnest/screens/home/card/CardView.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.bottomSheet.LocalBottomSheetNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import com.hrvs.cardnest.R
 import com.hrvs.cardnest.components.button.AppButton
@@ -22,14 +23,33 @@ data class CardViewScreen(val card: CardFullProfile) : Screen {
   @Composable
   override fun Content() {
     val navigator = LocalNavigator.currentOrThrow
+    val bottomSheetNavigator = LocalBottomSheetNavigator.current
+
+    fun edit() {
+      navigator.push(UpdateCardEditorScreen(card))
+    }
+
+    fun del() {
+      // TODO
+    }
+
+    fun showDeleteBottomSheet() {
+      bottomSheetNavigator.show(
+        DeleteCardBottomSheetScreen(
+          onConfirm = { del() },
+          onClose = { bottomSheetNavigator.hide() }
+        )
+      )
+    }
 
     SubScreenRoot(
       title = "Card",
       rightButtonLabel = "Edit",
       rightButtonIcon = painterResource(R.drawable.tabler__pencil),
-      onRightButtonClick = { navigator.push(UpdateCardEditorScreen(card)) },
+      onRightButtonClick = { edit() },
       spacedBy = 32.dp,
     ) {
+
       CardPreview(card)
       Column(verticalArrangement = Arrangement.spacedBy(24.dp)) {
         CopyableTextField(
@@ -46,7 +66,7 @@ data class CardViewScreen(val card: CardFullProfile) : Screen {
         }
       }
 
-      AppButton(title = "Delete", onClick = { /*TODO*/ }, ButtonTheme.Danger)
+      AppButton(title = "Delete", onClick = { showDeleteBottomSheet() }, ButtonTheme.Danger)
     }
   }
 }
