d5d1815c01f4ae3bf5d593c52d3979651b698b9b
Harsh Vyas <kumarvyasss@gmail.com>
2024-06-19 13:42:37 +0530

    Add delete functionality in `CardViewScreen`

--- app/src/main/java/com/hrvs/cardnest/screens/home/card/CardView.kt
+++ app/src/main/java/com/hrvs/cardnest/screens/home/card/CardView.kt
@@ -3,6 +3,8 @@ package com.hrvs.cardnest.screens.home.card
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
@@ -15,25 +17,37 @@ import com.hrvs.cardnest.components.button.ButtonTheme
 import com.hrvs.cardnest.components.card.CardPreview
 import com.hrvs.cardnest.components.containers.SubScreenRoot
 import com.hrvs.cardnest.components.core.CopyableTextField
-import com.hrvs.cardnest.data.CardFullProfile
+import com.hrvs.cardnest.data.serializables.CardRecord
 import com.hrvs.cardnest.screens.home.card.editor.UpdateCardEditorScreen
+import com.hrvs.cardnest.state.card.deleteCard
 import com.hrvs.cardnest.utils.card.addCardNumberSpaces
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-data class CardViewScreen(val card: CardFullProfile) : Screen {
+data class CardViewScreen(val cardRecord: CardRecord) : Screen {
   @Composable
   override fun Content() {
+    val ctx = LocalContext.current
     val navigator = LocalNavigator.currentOrThrow
     val bottomSheetNavigator = LocalBottomSheetNavigator.current
 
-    fun edit() {
+    val scope = rememberCoroutineScope()
+    val card = cardRecord.plainData
+
+    fun del() {
+      scope.launch {
+        bottomSheetNavigator.hide()
+        delay(200)
+        deleteCard(ctx, cardRecord.id)
+        navigator.pop()
+      }
+    }
+
+    fun onEditClick() {
       navigator.push(UpdateCardEditorScreen(card))
     }
 
-    fun del() {
-      // TODO
-    }
-
-    fun showDeleteBottomSheet() {
+    fun onDeleteClick() {
       bottomSheetNavigator.show(
         DeleteCardBottomSheetScreen(
           onConfirm = { del() },
@@ -46,7 +60,7 @@ data class CardViewScreen(val card: CardFullProfile) : Screen {
       title = "Card",
       rightButtonLabel = "Edit",
       rightButtonIcon = painterResource(R.drawable.tabler__pencil),
-      onRightButtonClick = { edit() },
+      onRightButtonClick = ::onEditClick,
       spacedBy = 32.dp,
     ) {
 
@@ -66,7 +80,7 @@ data class CardViewScreen(val card: CardFullProfile) : Screen {
         }
       }
 
-      AppButton(title = "Delete", onClick = { showDeleteBottomSheet() }, ButtonTheme.Danger)
+      AppButton(title = "Delete", onClick = ::onDeleteClick, ButtonTheme.Danger)
     }
   }
 }
