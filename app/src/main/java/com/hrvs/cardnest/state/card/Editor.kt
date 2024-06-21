02856be0780b49c974540753b827e74a1a905221
Harsh Vyas <kumarvyasss@gmail.com>
2024-06-21 16:05:01 +0530

    Update `CardEditorViewModel`

--- app/src/main/java/com/hrvs/cardnest/state/card/Editor.kt
+++ app/src/main/java/com/hrvs/cardnest/state/card/Editor.kt
@@ -1,10 +1,16 @@
 package com.hrvs.cardnest.state.card
 
 import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.hrvs.cardnest.data.CardholderError
+import com.hrvs.cardnest.data.CardErrorsState
+import com.hrvs.cardnest.data.CardExpiryError
 import com.hrvs.cardnest.data.CardFocusableField
 import com.hrvs.cardnest.data.CardFullProfile
+import com.hrvs.cardnest.data.CardNumberError
 import com.hrvs.cardnest.data.CardTheme
 import com.hrvs.cardnest.data.PaymentNetwork
 import com.hrvs.cardnest.utils.card.addCardNumberSpaces
@@ -20,6 +26,27 @@ class CardEditorViewModel(card: CardFullProfile) : ViewModel() {
 
   val focused = mutableStateOf<CardFocusableField?>(null)
 
+  private val hasSubmitted = mutableStateOf(false)
+
+  val errors by derivedStateOf {
+    if (!hasSubmitted.value) return@derivedStateOf CardErrorsState()
+    CardErrorsState(
+      number = CardNumberError(number.text.length != 19),
+      expiry = CardExpiryError(expiry.text.length != 5),
+      cardholder = CardholderError(cardholder.text.length < 2),
+    )
+  }
+
+  val card
+    get() = CardFullProfile(
+      number = number.text.toString(),
+      expiry = expiry.text.toString(),
+      cardholder = cardholder.text.toString(),
+      issuer = issuer.text.toString(),
+      network = network.value,
+      theme = theme.value,
+    )
+
   fun onNetworkChange(network: PaymentNetwork) {
     this.network.value = network
   }
@@ -32,13 +59,9 @@ class CardEditorViewModel(card: CardFullProfile) : ViewModel() {
     this.focused.value = focused
   }
 
-  val card
-    get() = CardFullProfile(
-      number = number.text.toString(),
-      expiry = expiry.text.toString(),
-      cardholder = cardholder.text.toString(),
-      issuer = issuer.text.toString(),
-      network = network.value,
-      theme = theme.value,
-    )
+  fun onSubmit(next: (data: CardFullProfile) -> Unit) {
+    hasSubmitted.value = true
+    if (errors.number.hasError || errors.expiry.hasError || errors.cardholder.hasError) return
+    next(card)
+  }
 }
