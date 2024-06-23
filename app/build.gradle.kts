2f0a9eff09d31229725c187cf315a8b0fd08b70b
Harsh Vyas <kumarvyasss@gmail.com>
2024-06-23 17:15:28 +0530

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
 
-  kotlin("plugin.serialization") version "1.9.0"
+  kotlin("plugin.serialization") version "2.0.0"
 }
 
 android {
@@ -54,7 +54,7 @@ dependencies {
   implementation("androidx.core:core-ktx:1.13.1")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
   implementation("androidx.activity:activity-compose:1.9.2")
-  implementation(platform("androidx.compose:compose-bom:2024.09.02"))
+  implementation(platform("androidx.compose:compose-bom:2024.09.03"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
@@ -68,7 +68,7 @@ dependencies {
   // Data
   implementation("androidx.datastore:datastore:1.1.1")
   implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
   // Id
   implementation("io.viascom.nanoid:nanoid:1.0.1")
