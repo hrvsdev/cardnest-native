4f45b35229a6cf2a30e6659eef440c027e57a539
Harsh Vyas <kumarvyasss@gmail.com>
2024-07-02 12:57:12 +0530

    Create `ViewModelFactory`

---
+++ app/src/main/java/com/hrvs/cardnest/state/ViewModelFactory.kt
@@ -0,0 +1,13 @@
+package com.hrvs.cardnest.state
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+fun <VM : ViewModel> appViewModelFactory(initializer: () -> VM): ViewModelProvider.Factory {
+  return object : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+      @Suppress("UNCHECKED_CAST")
+      return initializer() as T
+    }
+  }
+}
