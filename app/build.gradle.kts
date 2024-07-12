c63058b0b2f7b393a9a5462d9f37c1eb09cd53af
Harsh Vyas <kumarvyasss@gmail.com>
2024-07-12 11:19:39 +0530

    Add Koin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
   implementation(libs.voyager.bottom.sheet.navigation)
   implementation(libs.voyager.transitions)
   implementation(libs.voyager.screenmodel)
+  implementation(libs.koin.androidx.compose)
 
   runtimeOnly(libs.androidx.material)
 }
