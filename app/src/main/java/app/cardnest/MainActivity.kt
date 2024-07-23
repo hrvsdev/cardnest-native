03016047d46c7ad2b448a501138ec3a4f8c74a51
Harsh Vyas <kumarvyasss@gmail.com>
2024-07-23 12:19:14 +0530

    Show splash screen until `AuthDataViewModel` is not loaded

--- app/src/main/java/app/cardnest/MainActivity.kt
+++ app/src/main/java/app/cardnest/MainActivity.kt
@@ -11,11 +11,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.datastore.dataStore
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import app.cardnest.di.appModule
 import app.cardnest.state.AuthDataSerializer
 import app.cardnest.state.CardsDataSerializer
+import app.cardnest.state.auth.AuthDataViewModel
+import app.cardnest.state.card.State
 import app.cardnest.ui.theme.CardNestTheme
 import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.compose.koinViewModel
 import org.koin.androix.startup.KoinStartup.onKoinStartup
 import org.koin.core.annotation.KoinExperimentalAPI
 
@@ -25,10 +29,15 @@ val Context.authDataStore by dataStore("auth_data.json", AuthDataSerializer)
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    installSplashScreen()
+    val splashScreen = installSplashScreen()
     enableEdgeToEdge(SystemBarStyle.dark(Color.TRANSPARENT), SystemBarStyle.dark(Color.TRANSPARENT))
     setContent {
       CardNestTheme {
+        val koinVM = koinViewModel<AuthDataViewModel>()
+        val isLoading = koinVM.data.collectAsStateWithLifecycle().value is State.Loading
+
+        splashScreen.setKeepOnScreenCondition { isLoading }
+
         App()
       }
     }
