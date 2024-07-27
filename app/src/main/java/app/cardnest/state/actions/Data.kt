b50525382e4a51ddc1222455a894297de79cd3a7
Harsh Vyas <kumarvyasss@gmail.com>
2024-07-27 11:42:47 +0530

    Make `Action` non-nullable

--- app/src/main/java/app/cardnest/state/actions/Data.kt
+++ app/src/main/java/app/cardnest/state/actions/Data.kt
@@ -2,11 +2,11 @@ package app.cardnest.state.actions
 
 import androidx.lifecycle.ViewModel
 
-typealias Action = (() -> Unit)?
+typealias Action = (() -> Unit)
 
 class ActionsViewModel : ViewModel() {
-  private var _afterPinCreated: Action = null
-  private var _afterPinVerified: Action = null
+  private var _afterPinCreated: Action = { }
+  private var _afterPinVerified: Action = { }
 
   val afterPinCreated get() = _afterPinCreated
   val afterPinVerified get() = _afterPinVerified
