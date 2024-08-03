7e3b5c64e72169a7b8290b1932fc023c2137d8e1
Harsh Vyas <kumarvyasss@gmail.com>
2024-08-03 14:29:54 +0530

    Update `AuthData`

--- app/src/main/java/app/cardnest/data/serializables/Auth.kt
+++ app/src/main/java/app/cardnest/data/serializables/Auth.kt
@@ -4,7 +4,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AuthData(
-  val toCheck: String = "",
+  val salt: ByteArray? = null,
+  val encryptedRandomKey: EncryptedData? = null,
   val hasCreatedPin: Boolean = false,
   val hasBiometricsEnabled: Boolean = false
 )
+
+@Serializable
+data class EncryptedData(val ciphertext: ByteArray, val iv: ByteArray)
