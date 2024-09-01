9cfa46cc660188d0fbf421e607dde3aa1831af70
Harsh Vyas <kumarvyasss@gmail.com>
2024-09-01 17:40:06 +0530

    Add search query in `HomeViewModel`

--- app/src/main/java/app/cardnest/screens/home/HomeViewModel.kt
+++ app/src/main/java/app/cardnest/screens/home/HomeViewModel.kt
@@ -1,20 +1,39 @@
 package app.cardnest.screens.home
 
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import app.cardnest.data.card.Card
 import app.cardnest.data.card.CardDataManager
 import app.cardnest.data.cardsState
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class HomeViewModel(private val dataManager: CardDataManager) : ViewModel() {
-  val cardRecordList = cardsState.map { it.values.toList() }.stateIn(
+  val queryState = TextFieldState()
+
+  @OptIn(FlowPreview::class)
+  private val queryTextFlow = snapshotFlow { queryState.text }.debounce(150)
+
+  val cardRecordList = combine(cardsState, queryTextFlow) { state, text ->
+    state.values.filter { filterCard(it.plainData, text) }
+  }.stateIn(
     scope = viewModelScope,
     started = SharingStarted.WhileSubscribed(5000),
     initialValue = emptyList()
+  )
+
+  val totalNoOfCards = cardsState.map { it.size }.stateIn(
+    scope = viewModelScope,
+    started = SharingStarted.WhileSubscribed(5000),
+    initialValue = 0
   )
 
   init {
@@ -22,4 +41,16 @@ class HomeViewModel(private val dataManager: CardDataManager) : ViewModel() {
       dataManager.decryptAndCollectCards()
     }
   }
+
+  private fun filterCard(card: Card, query: CharSequence): Boolean {
+    if (query.isBlank()) return true
+    return searchableFields(card).any { it.contains(query.trim(), ignoreCase = true) }
+  }
+
+  private fun searchableFields(card: Card) = listOf(
+    card.network.toString(),
+    card.issuer,
+    card.cardholder,
+    card.number,
+  )
 }
