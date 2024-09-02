3fb5aba6688942f27059f33d896c26d496b2fd99
Harsh Vyas <kumarvyasss@gmail.com>
2024-09-02 13:55:35 +0530

    Add `AnnotatedString` in `AppText`

--- app/src/main/java/app/cardnest/ui/theme/Type.kt
+++ app/src/main/java/app/cardnest/ui/theme/Type.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -28,38 +29,6 @@ fun AppText(
   lineHeight: TextUnit = TextUnit.Unspecified,
   useCardFontFamily: Boolean = false
 ) {
-
-  val fontSize = when (size) {
-    AppTextSize.XXS -> 10.sp
-    AppTextSize.XS -> 12.sp
-    AppTextSize.SM -> 14.sp
-    AppTextSize.BASE -> 16.sp
-    AppTextSize.LG -> 18.sp
-    AppTextSize.XL -> 20.sp
-    AppTextSize.XXL -> 24.sp
-    AppTextSize.XXXL -> 30.sp
-    AppTextSize.MD -> 17.sp
-    AppTextSize.HEADING -> 28.sp
-  }
-
-  val actualLineHeight = when (lineHeight) {
-    TextUnit.Unspecified -> when (size) {
-      AppTextSize.XXS -> 12.sp
-      AppTextSize.XS -> 16.sp
-      AppTextSize.SM -> 20.sp
-      AppTextSize.BASE -> 24.sp
-      AppTextSize.LG -> 28.sp
-      AppTextSize.XL -> 28.sp
-      AppTextSize.XXL -> 32.sp
-      AppTextSize.XXXL -> 36.sp
-      AppTextSize.MD -> 26.sp
-      AppTextSize.HEADING -> 34.sp
-    }
-
-    else -> lineHeight
-  }
-
-
   BasicText(
     text = text,
     modifier = modifier,
@@ -68,8 +37,36 @@ fun AppText(
       fontWeight = weight,
       letterSpacing = letterSpacing,
       textAlign = align,
-      fontSize = fontSize,
-      lineHeight = actualLineHeight,
+      fontSize = size.fontSize,
+      lineHeight = if (lineHeight == TextUnit.Unspecified) size.lineHeight else lineHeight,
+      lineHeightStyle = LineHeightStyle(Alignment.Center, Trim.None),
+      fontFamily = if (useCardFontFamily) ManropeFamily else LatoFamily
+    )
+  )
+}
+
+@Composable
+fun AppText(
+  text: AnnotatedString,
+  modifier: Modifier = Modifier,
+  size: AppTextSize = AppTextSize.BASE,
+  weight: FontWeight = FontWeight.Normal,
+  align: TextAlign = TextAlign.Start,
+  color: Color = TH_WHITE_80,
+  letterSpacing: TextUnit = TextUnit.Unspecified,
+  lineHeight: TextUnit = TextUnit.Unspecified,
+  useCardFontFamily: Boolean = false
+) {
+  BasicText(
+    text = text,
+    modifier = modifier,
+    style = TextStyle(
+      color = color,
+      fontWeight = weight,
+      letterSpacing = letterSpacing,
+      textAlign = align,
+      fontSize = size.fontSize,
+      lineHeight = if (lineHeight != TextUnit.Unspecified) size.lineHeight else lineHeight,
       lineHeightStyle = LineHeightStyle(Alignment.Center, Trim.None),
       fontFamily = if (useCardFontFamily) ManropeFamily else LatoFamily
     )
@@ -89,6 +86,15 @@ val ManropeFamily = FontFamily(
   Font(R.font.manrope_bold, FontWeight.Bold),
 )
 
-enum class AppTextSize {
-  XXS, XS, SM, BASE, LG, XL, XXL, XXXL, MD, HEADING
+enum class AppTextSize(val fontSize: TextUnit, val lineHeight: TextUnit) {
+  XXS(10.sp, 12.sp),
+  XS(12.sp, 16.sp),
+  SM(14.sp, 20.sp),
+  BASE(16.sp, 24.sp),
+  LG(18.sp, 28.sp),
+  XL(20.sp, 28.sp),
+  XXL(24.sp, 32.sp),
+  XXXL(30.sp, 36.sp),
+  MD(17.sp, 26.sp),
+  HEADING(28.sp, 34.sp)
 }
