64cd6d2e60f42d227ef74345f18e80b77788592b
Harsh Vyas <kumarvyasss@gmail.com>
2024-11-07 14:11:35 +0530

    Manage errors in `UpdateCardViewModel`

--- app/src/main/java/app/cardnest/screens/home/card/editor/UpdateCardViewModel.kt
+++ app/src/main/java/app/cardnest/screens/home/card/editor/UpdateCardViewModel.kt
@@ -5,6 +5,10 @@ import androidx.lifecycle.viewModelScope
 import app.cardnest.data.card.Card
 import app.cardnest.data.card.CardDataManager
 import app.cardnest.data.card.CardUnencrypted
+import app.cardnest.screens.home.HomeScreen
+import app.cardnest.screens.home.card.CardViewScreen
+import app.cardnest.utils.extensions.toastAndLog
+import app.cardnest.utils.genId
 import cafe.adriel.voyager.navigator.Navigator
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,8 +20,12 @@ class UpdateCardViewModel(
 ) : ViewModel() {
   fun updateCard(id: String, card: Card) {
     viewModelScope.launch(Dispatchers.IO) {
-      dataManager.encryptAndAddOrUpdateCard(CardUnencrypted(id, card, System.currentTimeMillis()))
-      navigator.pop()
+      try {
+        dataManager.encryptAndAddOrUpdateCard(CardUnencrypted(id, card, System.currentTimeMillis()))
+        navigator.pop()
+      } catch (e: Exception) {
+        e.toastAndLog("UpdateCardViewModel")
+      }
     }
   }
 }
