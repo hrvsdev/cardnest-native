d3e11a8aaf7291ae81c7d1ff15ca0d509a85b7cc
Harsh Vyas <kumarvyasss@gmail.com>
2024-12-14 11:02:47 +0530

    Remove unnecessary code and add loading animation in `AppButton`

--- app/src/main/java/app/cardnest/components/button/AppButton.kt
+++ app/src/main/java/app/cardnest/components/button/AppButton.kt
@@ -1,14 +1,28 @@
 package app.cardnest.components.button
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,8 +31,6 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import app.cardnest.components.loader.LoadingIcon
-import app.cardnest.components.toast.AppToast
-import app.cardnest.data.connectionState
 import app.cardnest.ui.theme.AppText
 import app.cardnest.ui.theme.AppTextSize
 import app.cardnest.ui.theme.TH_RED
@@ -29,7 +41,6 @@ import app.cardnest.ui.theme.TH_SKY_10
 import app.cardnest.ui.theme.TH_SKY_20
 import app.cardnest.ui.theme.TH_WHITE
 import app.cardnest.ui.theme.TH_WHITE_70
-import app.cardnest.utils.extensions.collectValue
 
 @Composable
 fun AppButton(
@@ -40,57 +51,58 @@ fun AppButton(
   variant: ButtonVariant = ButtonVariant.Solid,
 
   isLoading: Boolean = false,
-  isDisabledIfOffline: Boolean = false,
   isDisabled: Boolean = false,
 ) {
-  val isOffline = isDisabledIfOffline && connectionState.collectValue().shouldWrite.not()
-
-  val isFullyDisabled = isDisabled || isLoading
-  val isVisuallyDisabled = isFullyDisabled || isOffline
-
   val interactionSource = remember { MutableInteractionSource() }
 
-  val containerColor = when (variant) {
-    ButtonVariant.Solid -> when (theme) {
-      ButtonTheme.Primary -> if (isVisuallyDisabled) TH_SKY_20 else TH_SKY
-      ButtonTheme.Danger -> if (isVisuallyDisabled) TH_RED_20 else TH_RED
-    }
+  val isFullyDisabled = isDisabled || isLoading
+  val isVisuallyDisabled = isFullyDisabled
 
-    ButtonVariant.Flat -> when (theme) {
-      ButtonTheme.Primary -> TH_SKY_10
-      ButtonTheme.Danger -> TH_RED_10
-    }
-  }
+  val containerColor by animateColorAsState(
+    when (variant) {
+      ButtonVariant.Solid -> when (theme) {
+        ButtonTheme.Primary -> if (isVisuallyDisabled) TH_SKY_20 else TH_SKY
+        ButtonTheme.Danger -> if (isVisuallyDisabled) TH_RED_20 else TH_RED
+      }
 
-  val textAndContentColor = when (variant) {
-    ButtonVariant.Solid -> if (isVisuallyDisabled) TH_WHITE_70 else TH_WHITE
-    ButtonVariant.Flat -> when (theme) {
-      ButtonTheme.Primary -> TH_SKY
-      ButtonTheme.Danger -> TH_RED
+      ButtonVariant.Flat -> when (theme) {
+        ButtonTheme.Primary -> TH_SKY_10
+        ButtonTheme.Danger -> TH_RED_10
+      }
     }
-  }
+  )
 
-  fun onButtonClick() {
-    if (isOffline) {
-      AppToast.error("You are offline")
-      return
+  val textAndContentColor by animateColorAsState(
+    when (variant) {
+      ButtonVariant.Solid -> if (isVisuallyDisabled) TH_WHITE_70 else TH_WHITE
+      ButtonVariant.Flat -> when (theme) {
+        ButtonTheme.Primary -> TH_SKY
+        ButtonTheme.Danger -> TH_RED
+      }
     }
-
-    onClick()
-  }
+  )
 
   fun Modifier.button() = this.clickable(
-    onClick = ::onButtonClick,
+    onClick = onClick,
     interactionSource = interactionSource,
     indication = ripple(color = { textAndContentColor }),
     enabled = isFullyDisabled.not(),
     role = Role.Button
   )
 
-  Box(Modifier.height(48.dp).fillMaxWidth().clip(RoundedCornerShape(14.dp)).background(containerColor).button(), Alignment.Center) {
-    if (isLoading) {
-      LoadingIcon(color = textAndContentColor, size = 26.dp)
-    } else {
+  val enter = fadeIn() + expandHorizontally() + slideInHorizontally { it }
+  val exit = fadeOut() + shrinkHorizontally() + slideOutHorizontally { it }
+
+  Box(Modifier.height(48.dp).fillMaxWidth().clip(RoundedCornerShape(14.dp)).background(containerColor).button()) {
+    Row(Modifier.fillMaxSize(), Arrangement.Center, Alignment.CenterVertically) {
+      AnimatedVisibility(isLoading, enter = enter, exit = exit) {
+        LoadingIcon(color = TH_WHITE_70)
+      }
+
+      AnimatedVisibility(isLoading, enter = expandHorizontally(), exit = shrinkHorizontally()) {
+        Spacer(Modifier.size(8.dp))
+      }
+
       AppText(title, size = AppTextSize.MD, weight = FontWeight.Bold, color = textAndContentColor)
     }
   }
